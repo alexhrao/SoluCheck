1dc2372c9b1a37a9de83d623073dcc8c94770d46
Alex Rao <alexhrao@gmail.com>
2016-10-20 13:48:34 -0400

    Fixing logic of readySubmit with respect to finding input fields.
    Hello World!

--- Support/CoreFiles/readySubmit.m
+++ Support/CoreFiles/readySubmit.m
@@ -38,27 +38,27 @@ try
         cellLines{intLine} = fgetl(fidMaster);
     end
     fclose(fidMaster);
-    strNLine = cellLines{strFound(cellLines, '% Name')};
-    strILine = cellLines{strFound(cellLines, '% T-square ID')};
-    strELine = cellLines{strFound(cellLines, '% GT Email')};
-    strHLine = cellLines{strFound(cellLines, '% Homework')};
-    strCLine = cellLines{strFound(cellLines, '% Course')};
-    strSLine = cellLines{strFound(cellLines, '% Section')};
-    strDLine = cellLines{strFound(cellLines, '% Collaboration')};
+    strNLine = cellLines{strFound(cellLines, '% Name', [], [], 1)};
+    strILine = cellLines{strFound(cellLines, '% T-square ID', [], [], 1)};
+    strELine = cellLines{strFound(cellLines, '% GT Email', [], [], 1)};
+    strHLine = cellLines{strFound(cellLines, '% Homework', [], [], 1)};
+    strCLine = cellLines{strFound(cellLines, '% Course', [], [], 1)};
+    strSLine = cellLines{strFound(cellLines, '% Section', [], [], 1)};
+    strDLine = cellLines{strFound(cellLines, '% Collaboration', [], [], 1   )};
     if ~isempty(strNLine)
         intPosn = strfind(strNLine, ':');
         strNLine = [strNLine(1:intPosn) ' ' strName];
-        cellLines{strFound(cellLines, '% Name')} = strNLine;
+        cellLines{strFound(cellLines, '% Name', [], [], 1)} = strNLine;
     end
     if ~isempty(strILine)
         intPosn = strfind(strILine, ':');
         strILine = [strILine(1:intPosn) ' ' strID];
-        cellLines{strFound(cellLines, '% T-square ID')} = strILine;
+        cellLines{strFound(cellLines, '% T-square ID', [], [], 1)} = strILine;
     end
     if ~isempty(strELine)
         intPosn = strfind(strELine, ':');
         strELine = [strELine(1:intPosn) ' ' strEmail];
-        cellLines{strFound(cellLines, '% GT Email')} = strELine;
+        cellLines{strFound(cellLines, '% GT Email', [], [], 1)} = strELine;
     end
     if ~isempty(strHLine)
         intPosn = strfind(strHLine, ':');
@@ -68,32 +68,32 @@ try
             strSub = 'Resubmission';
         end
         strHLine = [strHLine(1:intPosn) ' ' fstFile(3:strfind(fstFile, '.') - 1) '/' strSub];
-        cellLines{strFound(cellLines, '% Homework')} = strHLine;
+        cellLines{strFound(cellLines, '% Homework', [], [], 1)} = strHLine;
     end
     if ~isempty(strCLine)
         intPosn = strfind(strCLine, ':');
         strCLine = [strCLine(1:intPosn) ' ' strCID];
-        cellLines{strFound(cellLines, '% Course')} = strCLine;
+        cellLines{strFound(cellLines, '% Course', [], [], 1)} = strCLine;
     end
     if ~isempty(strSLine)
         intPosn = strfind(strSLine, ':');
         strSLine = [strSLine(1:intPosn) ' ' strSID];
-        cellLines{strFound(cellLines, '% Section')} = strSLine;
+        cellLines{strFound(cellLines, '% Section', [], [], 1)} = strSLine;
     end
     if ~isempty(strDLine)
         intPosn = strfind(strDLine, ':');
         strDLine = [strDLine(1:intPosn) ' ' 'I worked on the homework assignment alone, using only course materials.'];
-        cellLines{strFound(cellLines, '% Collaboration')} = strDLine;
-        intSPosn = find(strFound(cellLines, '% Collaboration'));
-        intEPosn = find(strFound(cellLines, '% Files provided with this homework:'));
+        cellLines{strFound(cellLines, '% Collaboration', [], [], 1)} = strDLine;
+        intSPosn = find(strFound(cellLines, '% Collaboration', [], [], 1));
+        intEPosn = find(strFound(cellLines, '% Files provided with this homework:', [], [], 1));
         cellLines(intSPosn+1:intEPosn-1) = [];
         cellLines = [cellLines(1:intSPosn) {'% '} cellLines(intSPosn+1:end)];
     end
     mkdir('HW_FilesToSubmit');
     strCurrFolder = cd;
     strPath = [strCurrFolder '\HW_FilesToSubmit'];
-    intStart = find(strFound(cellLines, 'Files to submit:')) + 1;
-    intFinish = find(strFound(cellLines, 'Instructions:')) - 2;
+    intStart = find(strFound(cellLines, 'Files to submit:', [], [], 1)) + 1;
+    intFinish = find(strFound(cellLines, 'Instructions:', [], [], 1)) - 2;
     cellFiles = cellLines(intStart:intFinish);
     cellPosn = num2cell(cellfun(@(c)(c(1)), cellfun(@(str)(regexp(str, '\w')), cellFiles, 'uni', false)));
     cellFiles = cellfun(@(str, pos)(str(pos:end)), cellFiles, cellPosn, 'uni', false);

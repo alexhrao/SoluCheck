989a4c513d583dcc32bc8081d7f02e04fe0fa299
Alexander Rao <ahrao@southerncompany.com>
2017-07-17 16:03:54 -0400

    Set up io page

--- Support/CoreFiles/anyMult.m
+++
@@ -1,9 +0,0 @@
-function logAny = anyMult(var)
-    % Same as any(var(:)), but works on things that are unlinearizable!
-    logAny = any(var);
-    if numel(logAny) == 1
-        return;
-    else
-        logAny = anyMult(logAny);
-    end
-end
--- Support/CoreFiles/stcNumel.m
+++
@@ -1,5 +0,0 @@
-function intTotal = stcNumel(stcVar)
-% Get the number of elements in a structure.
-cellFields = fieldnames(stcVar);
-intTotal = sum(cellfun(@(str)(numel(stcVar.(str))), cellFields));
-end
